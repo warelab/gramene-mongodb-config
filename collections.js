f498331b0a06cd2f93d8f9abd3bc55635a73068c
mycrobe <jmulvane@cshl.edu>
2015-10-09 16:27:38 -0400

    add mongo connect promise function for each collection; remove idea files from version control

--- collections.js
+++ collections.js
@@ -1,56 +1,80 @@
+'use strict';
+
+var _ = require('lodash');
+
 var host = 'brie.cshl.edu'
   , port = 27017
   , dbVersion = 46;
 
 var collections = {
-  genes : {
-    host           : host,
-    port           : port,
-    dbName         : 'search'+dbVersion,
-    collectionName : 'genes',
-    description    : 'gramene genes'
+  genes: {
+    host: host,
+    port: port,
+    dbName: 'search' + dbVersion,
+    collectionName: 'genes',
+    description: 'gramene genes'
   },
-  genetrees : {
-    host           : host,
-    port           : port,
-    dbName         : 'search'+dbVersion,
-    collectionName : 'genetree',
-    description    : 'compara gene trees'
-  }, 
-  GO : {
-    host           : host,
-    port           : port,
-    dbName         : 'ontology'+dbVersion,
-    collectionName : 'GO',
-    description    : 'gene ontology terms'
+  genetrees: {
+    host: host,
+    port: port,
+    dbName: 'search' + dbVersion,
+    collectionName: 'genetree',
+    description: 'compara gene trees'
   },
-  PO : {
-    host           : host,
-    port           : port,
-    dbName         : 'ontology'+dbVersion,
-    collectionName : 'PO',
-    description    : 'plant ontology terms'
+  GO: {
+    host: host,
+    port: port,
+    dbName: 'ontology' + dbVersion,
+    collectionName: 'GO',
+    description: 'gene ontology terms'
   },
-  taxonomy : {
-    host           : host,
-    port           : port,
-    dbName         : 'ontology'+dbVersion,
-    collectionName : 'NCBITaxon',
-    description    : 'NCBI taxonomy (pruned to cover gramene species)'
+  PO: {
+    host: host,
+    port: port,
+    dbName: 'ontology' + dbVersion,
+    collectionName: 'PO',
+    description: 'plant ontology terms'
   },
-  domains : {
-    host           : host,
-    port           : port,
-    dbName         : 'ontology'+dbVersion,
-    collectionName : 'interpro',
-    description    : 'intepro domains'
+  taxonomy: {
+    host: host,
+    port: port,
+    dbName: 'ontology' + dbVersion,
+    collectionName: 'NCBITaxon',
+    description: 'NCBI taxonomy (pruned to cover gramene species)'
   },
-  maps : {
-    host           : host,
-    port           : port,
-    dbName         : 'search'+dbVersion,
-    collectionName : 'maps',
-    description    : 'maps genomes, genetic maps, and physical maps'
+  domains: {
+    host: host,
+    port: port,
+    dbName: 'ontology' + dbVersion,
+    collectionName: 'interpro',
+    description: 'intepro domains'
+  },
+  maps: {
+    host: host,
+    port: port,
+    dbName: 'search' + dbVersion,
+    collectionName: 'maps',
+    description: 'maps genomes, genetic maps, and physical maps'
   }
 };
+
+// setup mongodb collection connections
+_.inject(collections, function (databases, collection) {
+  var MongoClient = require('mongodb').MongoClient;
+  var Q = require('q');
+
+  collection.mongoCollection = function () {
+    var database = databases[collection.dbName];
+    if (!database) {
+      var url = 'mongodb://' + collection.host + ':' + collection.port + '/' + collection.dbName;
+      database = databases[collection.dbName] = Q.ninvoke(MongoClient, "connect", url);
+    }
+
+    return database.then(function (db) {
+      return db.collection(collection.collectionName);
+    });
+  };
+  return databases;
+}, {});
+
 module.exports = collections;
