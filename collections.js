45cfa0a82653f5e806085bf0204aafc79386be3e
mycrobe <jmulvane@cshl.edu>
2015-11-05 10:47:26 -0500

    make collections a class so that we can attach methods to its prototype rather that that of Object. Clean up collections data structure.

--- collections.js
+++ collections.js
@@ -1,99 +1,71 @@
 'use strict';
 
 var _ = require('lodash');
+var Q = require('q');
+var MongoClient = require('mongodb').MongoClient;
 
 var host = 'brie.cshl.edu'
   , port = 27017
   , dbName = 'search'
-  , dbVersion = '48';
+  , dbVersion = '48'
+  , rootMongoUrl = 'mongodb://' + host + ':' + port + '/' + dbName + dbVersion
+  , databasePromise = Q.ninvoke(MongoClient, "connect", rootMongoUrl);
 
-var collections = {
+function Collections(collections) {
+  // copy all the properties to this object
+  _.assign(this, collections);
+
+  // add mongoCollection function to each.
+  _.forOwn(collections, function (collection) {
+    collection.mongoCollection = function() {
+      return databasePromise.then(function (db) {
+        return db.collection(collection.collectionName);
+      }).catch(function(err) {
+        console.log(err);
+      });
+    }.bind(this)
+  }.bind(this));
+}
+
+Collections.prototype.closeMongoDatabase = function () {
+  databasePromise.then(function (db) {
+    db.close();
+  });
+};
+
+var collections = new Collections({
   genes: {
-    host: host,
-    port: port,
-    dbName: dbName + dbVersion,
     collectionName: 'genes',
     description: 'gramene genes'
   },
   genetrees: {
-    host: host,
-    port: port,
-    dbName: dbName + dbVersion,
     collectionName: 'genetree',
     description: 'compara gene trees'
   },
   GO: {
-    host: host,
-    port: port,
-    dbName: dbName + dbVersion,
     collectionName: 'GO',
     description: 'gene ontology terms'
   },
   PO: {
-    host: host,
-    port: port,
-    dbName: dbName + dbVersion,
     collectionName: 'PO',
     description: 'plant ontology terms'
   },
   taxonomy: {
-    host: host,
-    port: port,
-    dbName: dbName + dbVersion,
     collectionName: 'NCBITaxon',
     description: 'NCBI taxonomy (pruned to cover gramene species)'
   },
   domains: {
-    host: host,
-    port: port,
-    dbName: dbName + dbVersion,
     collectionName: 'domains',
     description: 'intepro domains'
   },
   pathways: {
-    host: host,
-    port: port,
-    dbName: dbName + dbVersion,
     collectionName: 'pathways',
     description: 'plant reactome pathways and reactions'
   },
   maps: {
-    host: host,
-    port: port,
-    dbName: dbName + dbVersion,
     collectionName: 'maps',
     description: 'maps genomes, genetic maps, and physical maps'
   }
-};
-
-var databases = {};
-
-// setup mongodb collection connections
-_.forEach(collections, function (collection) {
-  var MongoClient = require('mongodb').MongoClient;
-  var Q = require('q');
-
-  collection.mongoCollection = function () {
-    var database = databases[collection.dbName];
-    if (!database) {
-      var url = 'mongodb://' + collection.host + ':' + collection.port + '/' + collection.dbName;
-      database = databases[collection.dbName] = Q.ninvoke(MongoClient, "connect", url);
-    }
-
-    return database.then(function (db) {
-      return db.collection(collection.collectionName);
-    });
-  };
 });
-
-Object.getPrototypeOf(collections).closeDatabases = function() {
-  _.forEach(databases, function(dbPromise) {
-    dbPromise.then(function(db) {
-      db.close();
-    });
-  });
-
-  databases = {};
-};
 
 module.exports = collections;
