a72e182ef6af51977b7fa2357463244d0a6b6c43
Andrew Olson <olson@cshl.edu>
2017-06-16 10:34:09 -0400

    release54

--- collections.js
+++ collections.js
@@ -7,7 +7,7 @@ var MongoClient = require('mongodb').MongoClient;
 var host = 'brie.cshl.edu'
   , port = 27017
   , dbName = 'search'
-  , dbVersion = '53'
+  , dbVersion = '54'
   , rootMongoUrl = 'mongodb://' + host + ':' + port + '/' + dbName + dbVersion
   , databasePromise = Q.ninvoke(MongoClient, "connect", rootMongoUrl);
 
