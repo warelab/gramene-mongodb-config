2c233721c2d934e94ae92016b4d20184ca797113
Andrew Olson <olson@cshl.edu>
2019-04-19 15:38:27 -0400

    grape2

--- collections.js
+++ collections.js
@@ -7,7 +7,7 @@ var MongoClient = require('mongodb').MongoClient;
 var host = 'brie.cshl.edu'
   , port = 27017
   , dbName = 'grape_search'
-  , dbVersion = '1'
+  , dbVersion = '2'
   , rootMongoUrl = 'mongodb://' + host + ':' + port + '/' + dbName + dbVersion
   , databasePromise = Q.ninvoke(MongoClient, "connect", rootMongoUrl);
 
