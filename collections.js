b8bad627b9ca9acd9e474f70e70f9e721514d694
Andrew Olson <olson@cshl.edu>
2020-02-21 08:34:23 -0500

    set database version

--- collections.js
+++ collections.js
@@ -7,7 +7,7 @@ var MongoClient = require('mongodb').MongoClient;
 var host = 'brie.cshl.edu'
   , port = 27017
   , dbName = 'maize_search'
-  , dbVersion = '3'
+  , dbVersion = '4'
   , rootMongoUrl = 'mongodb://' + host + ':' + port + '/' + dbName + dbVersion
   , databasePromise = Q.ninvoke(MongoClient, "connect", rootMongoUrl);
 
