651ab6d3c9e0938481d548aa7f8d2917195626ed
Andrew Olson <olson@cshl.edu>
2021-05-04 09:06:04 -0400

    maize pangenome 2

--- collections.js
+++ collections.js
@@ -7,7 +7,7 @@ var MongoClient = require('mongodb').MongoClient;
 var host = 'brie.cshl.edu'
   , port = 27017
   , dbName = 'maize_pangenome'
-  , dbVersion = '1'
+  , dbVersion = '2'
   , rootMongoUrl = 'mongodb://' + host + ':' + port + '/' + dbName + dbVersion
   , databasePromise = Q.ninvoke(MongoClient, "connect", rootMongoUrl);
 
