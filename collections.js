0929c09a279810f06b57c7b1be98aab67c1b2cdb
Andrew Olson <olson@cshl.edu>
2022-09-13 14:05:43 -0400

    update

--- collections.js
+++ collections.js
@@ -6,7 +6,7 @@ var MongoClient = require('mongodb').MongoClient;
 
 var host = 'localhost'
   , port = 27017
-  , dbName = 'sorghum
+  , dbName = 'sorghum'
   , dbVersion = '5'
   , rootMongoUrl = 'mongodb://' + host + ':' + port + '/' + dbName + dbVersion
   , databasePromise = Q.ninvoke(MongoClient, "connect", rootMongoUrl);
